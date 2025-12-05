78da5e78960ff4abb8629fa79e0964badfbf3906
JostinBasurtoM <jostin.basurto@integritysolutions.com.ec>
2025-12-05 10:29:57 -0500

    Agregacion de CORS

--- isc.bempleo.be.api/Program.cs
+++ isc.bempleo.be.api/Program.cs
@@ -3,6 +3,18 @@ using isc.bempleo.be.infrastructure.IOC;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("OrigenEspecificos", policy =>
+    {
+        policy.WithOrigins(allowedOrigins ?? Array.Empty<string>())
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
