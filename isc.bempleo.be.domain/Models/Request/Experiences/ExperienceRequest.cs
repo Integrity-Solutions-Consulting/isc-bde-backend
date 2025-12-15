881bb4d3b9ab77c1a0edce431cd48e2c3201fcb3
Ángel Vivanco <angel.vivanco@integritysolutions.com.ec>
2025-12-15 11:34:50 -0500

    Modificacion del tipo de dato a int del ExperienceTime

--- isc.bempleo.be.domain/Models/Request/Experiences/ExperienceRequest.cs
+++ isc.bempleo.be.domain/Models/Request/Experiences/ExperienceRequest.cs
@@ -10,7 +10,7 @@ namespace isc.bempleo.be.domain.Models.Request.Experiences
     {
         public string CompanyName { get; set; } = null!;
         public string PositionHeld { get; set; } = null!;
-        public string? ExperienceTime { get; set; }
+        public int? ExperienceTime { get; set; }
 
         public int ProfileId { get; set; }
     }
